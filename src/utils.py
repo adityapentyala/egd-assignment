40dcb70c08fecf7072c1032fc98c5145d0e114d4
Aditya Pentyala <adityapentyala02@gmail.com>
2024-02-10 12:45:14 +0530

    feat: add methods to print eq and params

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np 
+from tabulate import tabulate
 
 earliest_year=1985
 metrics = ["GDP (current LCU)", "GDP per capita (current US$)", "Gross savings (% of GDP)", 
@@ -20,4 +21,22 @@ def get_data(filepath: str, save: bool=False):
     filtered_data.to_excel(f"{filepath}_clean.xlsx")
     print("written succesfully!")
 
-    
+
+def print_equation(metrics, coeffs, intercept):
+    print('GDP growth rate = {:.2e}'.format(intercept), end=" ")
+    print("+", end=" ")
+    for i in range(len(metrics)):
+        print("{beta:.2e}({metric})".format(metric=metrics[i],beta=coeffs[i]), end=" ")
+        print("+", end=" ")
+    print()
+
+def print_stat_table(metrics, coeffs, intercept):
+    new_metrics = ['Y-Intercept']
+    new_metrics.extend(metrics)
+    betas = [intercept]
+    betas.extend(coeffs)
+    data = []
+    for i in range(len(new_metrics)):
+        data.append((new_metrics[i], betas[i]))
+    table = tabulate(data, headers=['METRIC', 'BETA'])
+    print(table)
