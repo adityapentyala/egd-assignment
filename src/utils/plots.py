d627e7fbc9d153ca480080279832a7fb3f65b0d5
Aditya Pentyala <adityapentyala02@gmail.com>
2024-04-22 20:43:56 +0530

    add regression lines for X

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -13,15 +13,18 @@ def create_plots(df):
         ax.xaxis.set_ticks(np.arange(start, end, 5))
         ax.set_xlabel("Year")
 
-def plot_vs_gdp(df):
+def plot_vs_gdp(df, growth_rate=False):
     r,c=3,2
+    to_plot = "GDP"
+    if growth_rate == True:
+        to_plot="gdp growth rate"
     cols = [["Natural Log of GDP per capita (current US$)", "Gross domestic savings (current US$)"],
             ["Labor force, total", "Net trade in goods and services (BoP, current US$)"],
             ["Consumer price index (2010 = 100)", "Foreign direct investment, net (BoP, current US$)"]]
     for i in range(r):
         for j in range(c):
             fig, axs = plt.subplots()
-            axs.scatter(df[cols[i][j]], df["GDP"])
-            axs.title.set_text(f"GDP vs {cols[i][j]}")
+            axs.scatter(df[cols[i][j]], df[to_plot])
+            axs.title.set_text(f"{to_plot} vs {cols[i][j]}")
             axs.set_xlabel(cols[i][j])
-            axs.set_ylabel("GDP")
+            axs.set_ylabel(to_plot)
