a717314331e0d0c2fc114c09845505e2c059648c
Aditya Pentyala <adityapentyala02@gmail.com>
2024-04-24 21:14:56 +0530

    added dummy variable plots

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -51,12 +51,18 @@ def plot_reg_lines(raw_df, X, y, model, dummy=False):
     else:
         for col in X.columns[1:7]:
             plt.figure()
-            pred_y = model.params[col]*raw_df[col] + model.params["const"] + model.params["developing_dummy"] + \
-                model.params["underdeveloped_dummy"] + mean_y - model.params[col]*means[col]
-            plt.plot(raw_df[col], pred_y, color="red")
-            plt.scatter(raw_df[col], pred_y, color="red")
+            dev_pred_y = model.params[col]*raw_df[col] + model.params["const"] + mean_y - model.params[col]*means[col]
+            underdev_pred_y = model.params[col]*raw_df[col] + model.params["const"] + model.params["underdeveloped_dummy"] + mean_y - model.params[col]*means[col]
+            developing_pred_y = model.params[col]*raw_df[col] + model.params["const"] + model.params["developing_dummy"] + mean_y - model.params[col]*means[col]
+            plt.plot(raw_df[col], dev_pred_y, color="red", label="developed")
+            plt.scatter(raw_df[col], dev_pred_y, color="red")
+            plt.plot(raw_df[col], underdev_pred_y, color="green", label="underdeveloped")
+            plt.scatter(raw_df[col], underdev_pred_y, color="green")
+            plt.plot(raw_df[col], developing_pred_y, color="darkviolet", label="developing")
+            plt.scatter(raw_df[col], developing_pred_y, color="darkviolet")
             plt.scatter(raw_df[col], raw_df["gdp growth rate"])
             plt.title(f"Fitted GDP growth rate vs {col}")
             plt.xlabel(col)
             plt.ylabel("GDP growth rate")
+            plt.legend()
             plt.show()
